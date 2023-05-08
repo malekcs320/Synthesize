051a0774e2b0521601eb5d5879694d28fe247af6
malekcs320 <malek.mahjoub1999@gmail.com>
2023-05-09 00:20:45 +0200

    ft: recommendation of local documents

--- synthesizer.py
+++ synthesizer.py
@@ -4,12 +4,14 @@ import os ,json
 from db import Database
 import pika
 from bson import ObjectId
+from utils_recom import distances_from_embeddings, indices_of_nearest_neighbors_from_distances
 
 class Synthesizer:
     def __init__(self):
         load_dotenv()
         db_uri = os.getenv("DB_URI")
         db = Database(db_uri, 'myDataBase').db
+        self.name = "synthesis"
         self.transcript_collection = db["transcripts"]
         self.notes_collection=db["notes"]
         self.synthesis_collection=db["synthesis"]
@@ -26,7 +28,11 @@ class Synthesizer:
         give me a 3 question quizz with answers \
         please generate it in a full stylized html format and differenciate answers from question. my fianl goal is to make the answers invesible at first and only show them when the user clicks on a button and make them visible when the user reclicks on it\
         the synthesis: \n {input_document} \n "
+        self.tags_prompt = " Extract keywords from this text:\n\n {input_document} \n "
         self.quizz=""
+        self.tags=""
+        self.embedding=[]
+        self.recommendations=""
         self.receive()
        
         
@@ -41,21 +47,69 @@ class Synthesizer:
             temperature=0,
         )
         return response.choices[0].text.strip()
-    def receive(self):
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-        channel = connection.channel()
-        channel.queue_declare(queue='synthesize')
-        def callback(ch, method, properties, body):
-            jsonbody=json.loads(body)
-            transcript_id=jsonbody["transcript_id"]
-            notes_id=jsonbody["notes_id"]
-            result=self.synthesize(transcript_id,notes_id)
-            self.synthesis_collection.insert_one({'name': 'first synthesis','text':result , 'transcript_id':transcript_id , 'notes_id':notes_id , 'quizz': self.quizz})
+    
 
-        channel.basic_consume(queue='synthesize', on_message_callback=callback, auto_ack=True)
+    def generate_quizz(self, document):
+        # Call the OpenAI API to generate a summary of the input text
+        response = openai.Completion.create(
+            engine=self.model_engine,
+            prompt=self.quizz_prompt.format(input_document=document),
+            max_tokens=1024,
+            n=1,
+            stop=None,
+            temperature=0,
+        )
+        return response.choices[0].text.strip()
+    
+    def generate_tags(self, document):
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=self.tags_prompt.format(input_document=document),
+            max_tokens=50,
+            top_p=1.0,
+            frequency_penalty=0.8,
+            presence_penalty=0.0,
+            temperature=0.5
+        )
+        return response.choices[0].text.strip()
 
-        print(' [*] Waiting for messages. To exit press CTRL+C')
-        channel.start_consuming()
+    def generate_tags_embeds(self, document):
+        tags = document.replace("\n", " ")
+        return openai.Embedding.create(input = [tags], model="text-embedding-ada-002")['data'][0]['embedding']
+
+    def generate_recommendations(self, tags):
+        recommendations = []
+        # retrieve embeddings with id from db
+        try:
+            embeddings = self.synthesis_collection.find({}, {"_id": 1, "embedding": 1})
+            embeddings = list(embeddings)
+        except StopIteration:
+            print("No synthesis found in db")
+            return []
+
+        ids_dic = dict()
+        for index in range(len(embeddings)):
+            ids_dic[index] = embeddings[index]["_id"]
+        # convert embeddings to list of lists
+        embeddings = [embedding["embedding"] for embedding in embeddings]
+        # generate embeddings for tags
+        self.embedding = self.generate_tags_embeds(tags)
+
+        # compute distances between tags and embeddings and keep only distances < 0.1
+        distances = distances_from_embeddings(self.embedding, embeddings)
+        distances = [distance for distance in distances if distance < 0.2]
+        print (f"length of recommendations {len(distances)}")
+        indices = []
+
+        if len(distances) >= 3:
+            indices = indices_of_nearest_neighbors_from_distances(distances)[:3]
+            _ids = [ids_dic[index] for index in indices]
+            # retrieve synthesis names with indices
+            recommendations = self.synthesis_collection.find({"_id": {"$in": _ids}}, {"_id": 1, "name": 1})
+            recommendations = list(recommendations)
+        return recommendations
+
+
     def synthesize(self,transcript_id,notes_id):
         
         # Retrieve transcript from db with transcript_id
@@ -84,23 +138,49 @@ class Synthesizer:
         f = open("static/synthesis.html", "w")
         f.write(document)
         f.close()
+        #generate tags
+        self.tags=self.generate_tags(document)
+        print(self.tags)
+        #generate recommendations
+        self.recommendations=self.generate_recommendations(self.tags)
+        f = open("static/recommendations.html", "w")
+        #convert recommendations to html
+        html = "<ul>"
+        for recommendation in self.recommendations:
+            html += f"<li>{recommendation['_id']}: {recommendation['name']}</li>"
+        html += "</ul>"
+        f.write(html)
+        f.close()
+        
+        print(self.recommendations)
+        # generate quizz
         self.quizz=self.generate_quizz(document)
         f = open("static/quizz.html", "w")
         f.write(self.quizz)
         f.close()
-        print(self.quizz)
+        # print(self.quizz)
         # Return synthesized document
         return document
-    def generate_quizz(self, document):
-        # Call the OpenAI API to generate a summary of the input text
-        response = openai.Completion.create(
-            engine=self.model_engine,
-            prompt=self.quizz_prompt.format(input_document=document),
-            max_tokens=1024,
-            n=1,
-            stop=None,
-            temperature=0,
-        )
-        return response.choices[0].text.strip()
+    
+    
+    
+    def receive(self):
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+        channel = connection.channel()
+        channel.queue_declare(queue='synthesize')
+
+        def callback(ch, method, properties, body):
+            jsonbody=json.loads(body)
+            transcript_id=jsonbody["transcript_id"]
+            notes_id=jsonbody["notes_id"]
+            result=self.synthesize(transcript_id,notes_id)
+            self.synthesis_collection.insert_one({'name': self.name,'text':result , 'transcript_id':transcript_id , 'notes_id':notes_id , 'quizz': self.quizz, 'tags': self.tags, 'embedding': self.embedding, 'recommendations': self.recommendations})
+
+        channel.basic_consume(queue='synthesize', on_message_callback=callback, auto_ack=True)
+
+        print(' [*] Waiting for messages. To exit press CTRL+C')
+        channel.start_consuming()
+    
+    
 if __name__ == "__main__":
     synthesizer=Synthesizer()
