ab7d395c60cedfed6edbb73d588e57bc662eda3a
malekcs320 <malek.mahjoub1999@gmail.com>
2023-05-09 19:44:59 +0200

    adapted code and db access

--- rabbitmq/senderTest.py
+++ rabbitmq/senderTest.py
@@ -1,11 +1,12 @@
 import pika
+from dotenv import load_dotenv
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.queue_declare(queue='synthesize')
+channel.queue_declare(queue='synthesis')
 
-channel.basic_publish(exchange='', routing_key='synthesize', body="{\"transcript_id\":\"64541f5872e4b3f6763ac874\",\"notes_id\" : \"645534ccd258c8a896b63c9f\"}")
-print(" [x] Sent 'Hello World!'")
+channel.basic_publish(exchange='', routing_key='synthesis', body="{\"transcript_id\":\"64541f5872e4b3f6763ac874\",\"notes_id\" : \"645534ccd258c8a896b63c9f\"}")
+print(" [x] Sent ids ")
 connection.close()
